caf6799a462dbbee4b09ccae4390d34fd536dc4e
Jaunty-Liu <15613469426@163.com>
2021-10-30 15:56:27 +0800

    返回数据

--- 1.js
+++ 1.js
@@ -2,9 +2,16 @@ var http=require('http');
 var serve=http.createServer();
 serve.on('request',function(request,response){
     console.log('收到请求来自'+request.url)
-    response.write('hello')
-    response.write('        nodejs')
-    response.end()
+    var products=[
+        {
+            name:'张三',
+            id:'000'
+        },{
+            name:'李四',
+            id:'111'
+        }
+    ]
+    response.end(JSON.stringify(products))
 })
 serve.listen(3000,function(){
     console.log('输出')
