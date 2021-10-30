9c73ca1985593d78993a71c6a9bc8e994ef05256
Jaunty-Liu <15613469426@163.com>
2021-10-30 15:38:55 +0800

    zaikk

--- 1.js
+++ 1.js
@@ -1,9 +1,11 @@
 var http=require('http');
 var serve=http.createServer();
-serve.on('request',function(){
-    console.log('收到请求')
+serve.on('request',function(request,response){
+    console.log('收到请求来自'+request.url)
+    response.write('hello')
+    response.write('        nodejs')
+    response.end()
 })
 serve.listen(3000,function(){
     console.log('输出')
 })
-console.log(123)
