d69fed13a3681f8a7bec895fafaab0866d6db909
Jaunty <15613469426@163.com>
2021-10-30 14:05:13 +0800

    试试

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 var http=require('http');
 var serve=http.createServer();
 serve.on('request',function(){
-    console.log('shoudaoqqongqiu')
+    console.log('收到请求')
 })
 serve.listen(3000,function(){
     console.log('23')
