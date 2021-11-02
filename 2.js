d633cee1b2efe134b79fe3b5b12593370c39ab4c
Jaunty-Liu <15613469426@163.com>
2021-11-02 20:51:39 +0800

    压缩

--- 2.js
+++ 2.js
@@ -1,14 +1,5 @@
 var fs=require('fs');
-var data='Batman!!!Batman!!!!!';
-var writeStream=fs.createWriteStream('test1.txt');
-writeStream.write(data,'utf-8');
-writeStream.end();
-writeStream.on('finish', function() {
-   console.log("写入完成。");
-});
-
-writeStream.on('error', function(err){
-  console.log(err.stack);
-});
-
-console.log("程序执行完毕");
+var zlib=require('zlib');fs.createReadStream('test1.txt')
+.pipe(zlib.createGzip())
+.pipe(fs.createWriteStream('test1.gz'))
+console.log("jaudone")
