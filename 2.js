e216000ca0dff22cbe3578a05b9c17a7279c50b5
Jaunty-Liu <15613469426@163.com>
2021-11-02 20:22:20 +0800

    流读取

--- 2.js
+++ 2.js
@@ -1,11 +1,18 @@
-var buffer1 = Buffer.from('ABC');
-var buffer2 = Buffer.from('ABCD');
-var result = buffer1.compare(buffer2);
+const { chunk } = require("underscore");
 
-if(result < 0) {
-   console.log(buffer1 + " 在 " + buffer2 + "之前");
-}else if(result == 0){
-   console.log(buffer1 + " 与 " + buffer2 + "相同");
-}else {
-   console.log(buffer1 + " 在 " + buffer2 + "之后");
-}
+var fs=require('fs');
+var data='';
+var readStream=fs.createReadStream('test1.txt');
+readStream.setEncoding('UTF8');
+readStream.on('data',(chunk)=>{
+   data+=chunk;
+});
+readStream.on('end',function(){
+   console.log(data);
+});
+
+readStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("程序执行完毕");
