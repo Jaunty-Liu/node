f2f3f511f68b13e353167063c4fc8ac2212c4cf6
Jaunty-Liu <15613469426@163.com>
2021-11-02 16:04:22 +0800

    buffer

--- 2.js
+++ 2.js
@@ -1,9 +1,2 @@
-var events=require('events');
-var eventEmitter=new events.EventEmitter();
-eventEmitter.on('someEvent',function(msg1,msg2){
-  console.log('111',msg1,msg2);  
-});
-eventEmitter.on('someEvent',function(msg1,msg2){
-    console.log('222',msg1,msg2);
-});
-eventEmitter.emit('someEvent','!!!','@@@@');
+var buf=Buffer.from('hello','ascii');
+console.log(buf)
