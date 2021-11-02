0b82bb05de743c6b7e8c6c28fee068091348705c
Jaunty-Liu <15613469426@163.com>
2021-11-02 14:52:24 +0800

    监听示例

--- 2.js
+++ 2.js
@@ -1,6 +1,9 @@
 var events=require('events');
 var eventEmitter=new events.EventEmitter();
-eventEmitter.on('connection',function(){
-    console.log('1111');
+eventEmitter.on('someEvent',function(msg1,msg2){
+  console.log('111',msg1,msg2);  
 });
-eventEmitter.emit('connection');
+eventEmitter.on('someEvent',function(msg1,msg2){
+    console.log('222',msg1,msg2);
+});
+eventEmitter.emit('someEvent','!!!','@@@@');
