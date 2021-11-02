dc3becef38f702c8562c6acbbb8bdb7e89251ede
Jaunty-Liu <15613469426@163.com>
2021-11-02 17:10:39 +0800

    比较

--- 2.js
+++ 2.js
@@ -1,15 +1,11 @@
-const buf = Buffer.from([0x1, 0x2, 0x3, 0x4, 0x5]);
-const json = JSON.stringify(buf);
+var buffer1 = Buffer.from('ABC');
+var buffer2 = Buffer.from('ABCD');
+var result = buffer1.compare(buffer2);
 
-// 输出: {"type":"Buffer","data":[1,2,3,4,5]}
-console.log(json);
-
-const copy = JSON.parse(json, (key, value) => {
-  return value && value.type === 'Buffer' ?
-    Buffer.from(value.data) :
-    value;
-});
-console.log(JSON.stringify(copy));
-
-// 输出: <Buffer 01 02 03 04 05>
-console.log(copy);
+if(result < 0) {
+   console.log(buffer1 + " 在 " + buffer2 + "之前");
+}else if(result == 0){
+   console.log(buffer1 + " 与 " + buffer2 + "相同");
+}else {
+   console.log(buffer1 + " 在 " + buffer2 + "之后");
+}
