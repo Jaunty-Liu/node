1a3cc846efeeff7507ade38a9756ba669824bc29
Jaunty-Liu <15613469426@163.com>
2021-11-03 21:35:07 +0800

    web客户端

--- 2.js
+++ 2.js
@@ -1,20 +1,17 @@
-var http = require("http");
-var url = require("url");
- 
-function start(route) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
- 
-    route(pathname);
- 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
- 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+var http=require('http');
+var options={
+    host:'127.0.0.1',
+    port:'3000',
+    path:'/1.html'
 }
- 
-exports.start = start;
+var callback=(res)=>{   
+    var body='';
+    res.on('data',(chunk)=>{
+        body+=chunk;
+    });
+    res.on('end',()=>{
+        console.log(body);
+    });
+}
+var req=http.request(options,callback);
+req.end();
