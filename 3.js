2e953f6c7d34a9fb5878c1c8c8eb62d63040b9bf
Jaunty-Liu <15613469426@163.com>
2021-11-03 19:21:42 +0800

    读取文件

--- 3.js
+++ 3.js
@@ -1,5 +1,11 @@
-function main(argv) {
-    console.log(argv)
-  }
-          
-  main(process.argv)
+var fs = require("fs");
+
+// 异步打开文件
+console.log("准备打开文件！");
+fs.open('test1.txt', 'r+', function(err, fd) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log(fd);
+  console.log("文件打开成功！");
+});
