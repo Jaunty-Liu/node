600bc0b94ec051ebaffd32341667c9cec20d9b34
Jaunty-Liu <15613469426@163.com>
2021-11-03 11:07:43 +0800

    继承

--- 2.js
+++ 2.js
@@ -1,5 +1,23 @@
-var fs=require('fs');
-var zlib=require('zlib');fs.createReadStream('test1.txt')
-.pipe(zlib.createGzip())
-.pipe(fs.createWriteStream('test1.gz'))
-console.log("jaudone")
+var util = require('util'); 
+function Base() { 
+    this.name = 'base'; 
+    this.base = 1991; 
+    this.sayHello = function() { 
+    	console.log('Hello ' + this.name); 
+    }; 
+} 
+Base.prototype.showName = function() { 
+    console.log(this.name);
+}; 
+function Sub() { 
+    this.name = 'sub'; 
+} 
+util.inherits(Sub, Base); //继承
+var objBase = new Base(); 
+objBase.showName(); //base 
+objBase.sayHello(); //Hello base 
+console.log(objBase); //{ name: 'base', base: 1991, sayHello: [Function] } 
+var objSub = new Sub(); 
+objSub.showName(); //sub
+//objSub.sayHello(); 
+console.log(objSub); //{ name: 'sub' }
