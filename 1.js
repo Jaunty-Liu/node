8fd2813c72a124d36d255bf432b81ddcfe0fc827
Jaunty-Liu <15613469426@163.com>
2021-11-04 11:11:12 +0800

    express获取地址

--- 1.js
+++ 1.js
@@ -1,5 +1,16 @@
-function route(pathname) {
-    console.log("About to route a request for " + pathname);
-  }
-   
-  exports.route = route;
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+var server = app.listen(8081, "127.0.0.1",function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
