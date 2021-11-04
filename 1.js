cd1daa550f3a7f13d2ed9439d82d1ba06fc5710b
Jaunty-Liu <15613469426@163.com>
2021-11-04 17:04:48 +0800

    app.param

--- 1.js
+++ 1.js
@@ -1,21 +1,15 @@
 var express=require('express');
 var app=express();
-var bodyParser=require('body-parser');
-var urlencodedParser=bodyParser.urlencoded({extended:false});
-app.use('/public',express.static('public'));
-app.get('/1.html',(req,res)=>{
-  res.sendFile(__dirname+"/"+"1.html");
-})
-app.post('/progress_post',urlencodedParser,(req,res)=>{
-  var response={
-    "first_name":req.body.first_name,
-    "last_name":req.body.last_name
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-})
-var server=app.listen(8081,"127.0.0.1",()=>{
-  var host=server.address().address;
-  var port=server.address().port;
-  console.log("http://%s:%s",host,port);
-})
+app.param(['id', 'page'], function(req, res, next, value) {
+  console.log('param',value);
+  next();
+});
+app.get('/user/:id/:page', function(req, res, next) {
+  console.log('get1',req.params);
+  next();
+});
+app.get('/user/:id/:page', function (req, res, next) {
+  console.log('get2',req.params);
+  res.end();
+});
+app.listen(3000);
