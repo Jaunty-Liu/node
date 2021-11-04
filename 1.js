2db8139431c9595a95b88ab600343f6ea90f9b63
Jaunty-Liu <15613469426@163.com>
2021-11-04 13:11:15 +0800

    post提交数据

--- 1.js
+++ 1.js
@@ -1,16 +1,21 @@
-//express_demo.js 文件
-var express = require('express');
-var app = express();
- 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+var express=require('express');
+var app=express();
+var bodyParser=require('body-parser');
+var urlencodedParser=bodyParser.urlencoded({extended:false});
+app.use('/public',express.static('public'));
+app.get('/1.html',(req,res)=>{
+  res.sendFile(__dirname+"/"+"1.html");
 })
- 
-var server = app.listen(8081, "127.0.0.1",function () {
- 
-  var host = server.address().address
-  var port = server.address().port
- 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
- 
+app.post('/progress_post',urlencodedParser,(req,res)=>{
+  var response={
+    "first_name":req.body.first_name,
+    "last_name":req.body.last_name
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
+var server=app.listen(8081,"127.0.0.1",()=>{
+  var host=server.address().address;
+  var port=server.address().port;
+  console.log("http://%s:%s",host,port);
 })
