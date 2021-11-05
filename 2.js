336c04eda7f55061682f6d658e81a332d9c4ec4c
Jaunty-Liu <15613469426@163.com>
2021-11-05 19:50:40 +0800

    .exec验证

--- 2.js
+++ 2.js
@@ -2,13 +2,14 @@ var fs=require('fs');
 var child_process=require('child_process');
 for(var i=0;i<3;i++){
     var workerProcess=child_process.exec('node 1.js '+i,function(error,stdout,stderr){
+        console.log('jau');
         if (error) {
             console.log(error.stack);
             console.log('Error code: '+error.code);
             console.log('Signal received: '+error.signal);
         }
-        console.log('stdout'+stdout);
-        console.log('stderr'+stderr);
+        console.log('stdout：'+stdout);
+        console.log('stderr：'+stderr);
     });
     workerProcess.on('exit',function(code){
         console.log('子程序已退出，退出码'+code);
