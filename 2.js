feba15c2d14fe866cd5923156e7665d936d72158
Jaunty-Liu <15613469426@163.com>
2021-11-05 21:32:55 +0800

    .spawn

--- 2.js
+++ 2.js
@@ -1,17 +1,14 @@
 var fs=require('fs');
 var child_process=require('child_process');
 for(var i=0;i<3;i++){
-    var workerProcess=child_process.exec('node 1.js '+i,function(error,stdout,stderr){
-        console.log('jau');
-        if (error) {
-            console.log(error.stack);
-            console.log('Error code: '+error.code);
-            console.log('Signal received: '+error.signal);
-        }
-        console.log('stdout：'+stdout);
-        console.log('stderr：'+stderr);
+    var workerProcess=child_process.spawn('node',['1.js',i]);
+    workerProcess.stdout.on('data',(data)=>{
+        console.log('stdout：'+data);
     });
-    workerProcess.on('exit',function(code){
-        console.log('子程序已退出，退出码'+code);
+    workerProcess.stderr.on('data',(data)=>{
+        console.log('stderr：'+data);
+    });
+    workerProcess.on('close',(code)=>{
+        console.log('子进程已退出，退出码'+code);
     });
 }
