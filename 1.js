ac9be9d779376051e3a205e2495bdc532e9e0459
Jaunty-Liu <15613469426@163.com>
2021-11-05 19:09:34 +0800

    .use

--- 1.js
+++ 1.js
@@ -1,7 +1,11 @@
-var express = require('express'); 
-var app = express(); 
-var PORT = 3000; 
-  
-// View engine setup 
-app.set('Batman', 'redhood'); 
-console.log(app.get('Batman'));
+var express=require('express');
+var app = express();
+// this middleware will not allow the request to  go beyond it
+app.use(function(req, res, next) {
+    res.send('Hello World');
+});
+// this middleware will never reach this route
+app.use('/', function(req, res) {
+    res.send('Welcome');
+});
+app.listen(8080);
